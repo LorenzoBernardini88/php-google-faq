30cd4acc6ab631a20cb15d881d3157a774186494
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-02-16 15:35:45 +0100

    creo struttra html

--- index.php
+++ index.php
@@ -8,6 +8,28 @@
     <title>google-faq</title>
 </head>
 <body>
-    
+    <header>
+        <div class="top_head">
+            <img src="logo.png" alt="">
+            <p>Privacy e Termini</p>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Introduzione</a></li>
+                <li><a href="#">Norme sulla privacy</a></li>
+                <li><a href="#">Termini di Servizio</a></li>
+                <li><a href="#">Tecnologie</a></li>
+                <li><a href="#">Domande frequenti</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="testo">
+                <h2></h2>
+                <p></p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
